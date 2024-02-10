6cf6634acbf6cc201223cbadfeada826764ca5b3
MatteoBertoldo <89697695+MatteoBertoldo7@users.noreply.github.com>
2024-02-10 20:01:01 +0100

    Update slice_setup.sh

--- slice_setup.sh
+++ slice_setup.sh
@@ -1,4 +1,4 @@
-ryu-manager publicsafety.py --observe-links --ofp-tcp-listen-port 6633 &
+ryu-manager security.py --observe-links --ofp-tcp-listen-port 6633 &
 ryu-manager smarttraffic.py --observe-links --ofp-tcp-listen-port 6634 &
 ryu-manager iot.py --observe-links --ofp-tcp-listen-port 6635 &
 ryu-manager wifi.py --observe-links --ofp-tcp-listen-port 6636 &
